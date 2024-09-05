d6afa821b94fed819910b7da978424076c3d5fea
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-09-05 12:46:09 +0300

    add validation to signup and login

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 const { getHash, compareHash } = require('../src/crypt');
 const collection = require('../src/dbconfig');
@@ -21,23 +22,42 @@ router.get('/sign-up', (request, result) => {
     }
 });
 
-router.post('/sign-up', async (request, result) => {
-    let data = {
-        username: request.body['sign-up-username'],
-        hash: await getHash(request.body['sign-up-password']),
-    }
+router.post(
+    '/sign-up',
+    body('sign-up-username').toLowerCase().trim().notEmpty().isLength({ min: 3, max: 25 }).escape(),
+    // body('sign-up-password').trim().notEmpty().isLength({ min: 8 }),
+    async (request, result) => {
+        let data = {
+            username: request.body['sign-up-username'],
+            hash: await getHash(request.body['sign-up-password']),
+        }
 
-    if (await collection.findOne({ username: data.username })) {
-        result.render('sign-up', { alerts: [{ content: 'Username already taken', type: 'error' }] })
-    } else {
+        if (request.session.user) {
+            request.flash('alerts', [{ content: 'You are already logged in', type: 'info' }]);
+            result.redirect('/');
+            return;
+        }
+
+        const validation = validationResult(request);
+
+        if (!validation.isEmpty()) {
+            result.render('sign-up', { alerts: [{ content: 'Invalid username or password', type: 'error' }] });
+            return;
+        }
+
+        if (await collection.findOne({ username: data.username })) {
+            result.render('sign-up', { alerts: [{ content: 'Username already taken', type: 'error' }] });
+            return;
+        }
+
         data.memberSince = Date.now();
         data.showMemberSince = true;
+
         const userData = await collection.insertMany(data);
         console.log(`User "${userData[0].username}" created`);
         request.flash('alerts', [{ content: 'Account created successfully', type: 'success' }]);
         result.redirect('/account/log-in');
-    }
-});
+    });
 
 router.get('/log-in', (request, result) => {
     if (request.session.user) {
@@ -48,26 +68,37 @@ router.get('/log-in', (request, result) => {
     }
 });
 
-router.post('/log-in', async (request, result) => {
-    let data = {
-        username: request.body['log-in-username'],
-        password: request.body['log-in-password'],
-        keepLogged: request.body['log-in-stay'] == 'on' ? true : false,
-    }
+router.post(
+    '/log-in',
+    body('log-in-username').trim().notEmpty().escape(),
+    body('log-in-password').trim().notEmpty(),
+    async (request, result) => {
+        let data = {
+            username: request.body['log-in-username'],
+            password: request.body['log-in-password'],
+            keepLogged: request.body['log-in-stay'] == 'on' ? true : false,
+        }
 
-    const user_check = await collection.findOne({ username: data.username });
+        const validation = validationResult(request);
 
-    if (user_check && await compareHash(data.password, user_check.hash)) {
-        request.session.user = { id: user_check._id, username: data.username };
-        request.session.keepLogged = data.keepLogged;
-        request.session.timestamp = Date.now();
-        console.log(`User "${data.username}" logged in`);
-        request.flash('alerts', [{ content: 'Logged in successfully', type: 'success' }]);
-        result.redirect('/');
-    } else {
-        result.render('log-in', { alerts: [{ content: 'Invalid username or password', type: 'error' }] });
-    }
-});
+        if (!validation.isEmpty()) {
+            result.render('log-in', { alerts: [{ content: 'Invalid username or password', type: 'error' }] });
+            return;
+        }
+
+        const user_check = await collection.findOne({ username: data.username });
+
+        if (user_check && await compareHash(data.password, user_check.hash)) {
+            request.session.user = { id: user_check._id, username: data.username };
+            request.session.keepLogged = data.keepLogged;
+            request.session.timestamp = Date.now();
+            console.log(`User "${data.username}" logged in`);
+            request.flash('alerts', [{ content: 'Logged in successfully', type: 'success' }]);
+            result.redirect('/');
+        } else {
+            result.render('log-in', { alerts: [{ content: 'Invalid username or password', type: 'error' }] });
+        }
+    });
 
 router.get('/log-out', async (request, result) => { // convert to post
     request.session.user = null;

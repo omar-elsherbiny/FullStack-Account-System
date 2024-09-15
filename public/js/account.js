1e489805ebcc1dab29f33155570e9161ff2f4c03
ZiedDev <73042811+ZiedDev@users.noreply.github.com>
2024-09-15 12:08:33 +0300

    style the change username and password modal

--- public/js/account.js
+++ public/js/account.js
@@ -6,6 +6,10 @@ const backdrop = document.getElementById('backdrop');
 const usernameEditModal = document.getElementById('username-edit-modal');
 const passwordEditModal = document.getElementById('password-edit-modal');
 
+function closeModal() {
+    modalContainer.classList.add('hide');
+}
+
 accountFormUsernameEdit.addEventListener('click', e => {
     modalContainer.classList.remove('hide');
     usernameEditModal.classList.remove('hide');
@@ -22,6 +26,9 @@ document.addEventListener('keyup', e => {
     if (e.key == 'Escape') closeModal();
 });
 
-function closeModal() {
-    modalContainer.classList.add('hide');
-}
+// username change modal
+const usernameChangeCancel = document.getElementById('username-change-cancel');
+const passwordChangeCancel = document.getElementById('password-change-cancel');
+
+usernameChangeCancel.addEventListener('click', closeModal);
+passwordChangeCancel.addEventListener('click', closeModal);

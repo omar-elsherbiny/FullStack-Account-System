148d85f0f82a67d1788995f154ae1c72786a5359
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2024-09-22 16:04:12 +0300

    enviroment error messages

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,5 +1,13 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const connect = mongoose.connect('mongodb://localhost:27017/Account_System');
+
+['mongo_url'].forEach(envVar => {
+    if (!process.env[envVar]) {
+        throw Error(`Missing enviroment variable: ${envVar}`);
+    }
+});
+
+const connect = mongoose.connect(process.env.mongo_url);
 
 connect
     .then(() => {
--- src/session.js
+++ src/session.js
@@ -2,16 +2,22 @@ require('dotenv').config();
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
+['session_secret_key', 'mongo_url'].forEach(envVar => {
+    if (!process.env[envVar]) {
+        throw Error(`Missing enviroment variable: ${envVar}`);
+    }
+});
+
 module.exports = session({
     name: 'mongoSession',
-    secret: process.env.session_secret_key || 'secret',
+    secret: process.env.session_secret_key,
     httpOnly: true,
     secure: true,
     maxAge: 1000 * 60 * 60 * 24 * 30,
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl: 'mongodb://localhost:27017/Account_System'
+        mongoUrl: process.env.mongo_url
     }),
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 30
